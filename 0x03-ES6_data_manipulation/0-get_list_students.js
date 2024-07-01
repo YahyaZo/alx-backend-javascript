ccf93a0a6f44cd37bb035757062aa0e53f1479be
YahyaZo <khayoussef.yahya20@gmail.com>
2024-07-01 12:52:04 +0100

    Initial commit

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents(){
+	const attr1 = {
+		id : 1,
+		firstName : Guillaume,
+		location : San Francisco,
+	};
+	const attr2 = {
+                id : 2,
+                firstName : James,
+                location : Columbia,
+        };
+	const attr3 = {
+                id :3,
+                firstName : Serena,
+                location : San Francisco,
+        };
+	return [ attr1, attr2, attr3 ];
+}
+
